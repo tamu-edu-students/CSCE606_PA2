5a07c1f36cc420b8305afdc4669dbc532927b985
Zhicheng Chen <zchengchen0411@gmail.com>
2024-09-08 16:33:20 -0500

    add style and table

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,18 +11,17 @@
 # Seed the RottenPotatoes DB with some movies.
 more_movies = [
   {:title => 'My Neighbor Totoro', :rating => 'G',
-    :release_date => '16-Apr-1988'},
-  {:title => 'Green Book', :rating => 'PG-13',
-    :release_date => '16-Nov-2018'},
+    :release_date => '16-Apr-1988', :description => 'My Neighbor Totoro is a 1988 Japanese animated fantasy film written and directed by Hayao Miyazaki and animated by Studio Ghibli for Tokuma Shoten.'},
+  {:title => 'Green Book', :rating => 'PG-13', 
+    :release_date => '16-Nov-2018', :description => 'Green Book is a 2018 American biographical comedy-drama film directed by Peter Farrelly.'},
   {:title => 'Parasite', :rating => 'R',
-    :release_date => '30-May-2019'},
+    :release_date => '30-May-2019', :description => 'Parasite (Korean: 기생충; RR: Gisaengchung) is a 2019 South Korean dark comedy[8] thriller film directed by Bong Joon-ho, who co-wrote the film with Han Jin-won.'},
   {:title => 'Nomadland', :rating => 'R',
-    :release_date => '19-Feb-2021'},
+    :release_date => '19-Feb-2021', :description => 'A woman in her sixties, after losing everything in the Great Recession, embarks on a journey through the American West, living as a van-dwelling modern-day nomad.'},
   {:title => 'CODA', :rating => 'PG-13',
-    :release_date => '13-Aug-2021'}
+    :release_date => '13-Aug-2021', :description => 'CODA is a 2021 coming-of-age comedy-drama film written and directed by Sian Heder.'}
 ]
 
 more_movies.each do |movie|
-  # Movie.create!(movie)
   Movie.where(movie).first_or_create
 end

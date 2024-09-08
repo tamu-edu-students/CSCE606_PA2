d591dbac8dafadd87f60aad0fa9de87a1623249c
Zhicheng Chen <zchengchen0411@gmail.com>
2024-09-08 18:18:06 -0500

    finsh sort

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -3,7 +3,14 @@ class MoviesController < ApplicationController
 
   # GET /movies or /movies.json
   def index
-    @movies = Movie.order(params[:sort])
+    if params[:sort] == nil
+      @moives = Moives.all
+      return
+    end
+    if params[:sort_direct] == nil
+      params[:sort_direct] = "desc"
+    end
+    @movies = Movie.order(params[:sort] + " " + params[:sort_direct])
   end
 
   # GET /movies/1 or /movies/1.json
